c590e4a1021d4eeaa22ad9beff27aeb7ee6a7448
Bharadwaj Chavan <150778401+codebharatGG@users.noreply.github.com>
2023-11-15 05:39:05 +0000

    variables

---
+++ js/variables.js
@@ -0,0 +1,11 @@
+const accountId = 252525
+let accountName = "Bharadwaj"
+let accountEmail = "bhatradwajchavhan0@gmail.com"
+let accountCity = "Pune"
+let accountState = "Defualt"
+
+// accountId = 252525
+accountEmail = "bharat0@gmail.com"
+accountCity = "Nandurbar"
+accountName = "Chetan"
+console.table([accountId,accountEmail,accountCity,accountState,accountName])
