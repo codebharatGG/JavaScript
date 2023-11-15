ec058c6eb78482bb1bbf8487599198e408700462
Bharadwaj Chavan <150778401+codebharatGG@users.noreply.github.com>
2023-11-15 09:50:26 +0000

    Datatypes

---
+++ js/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // To treat all JS code as newer version
+//  x = alert(3 + 3) // We are using node js not browsser 
+
+// DATATYPES=>
+
+let name = "Bharadwaj"
+let age = 19
+let isLoggedin = true // Boolean types 
+
+/* 
+1) number => 2**53
+2) bigint => when we deal with a website who has more traffic 
+3) string => "Bharadwaj"
+4) boolean => true/false (haa ya naa)
+5) undefined
+6) null => stand alone value it an object 
+7) symbol => we will mostly used it when we learn react, it is use to find the uniqunness of the programme
+8) objects 
+*/
+
+// To find the type of any data
+console.log(typeof 3);
+console.log(typeof "bharadwaj");
+console.log(typeof true);
+console.log(typeof isLoggedin);
+
+// ASSIGNMENT read all the specification on ecma script
