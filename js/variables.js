84816f30e44cfd79eb92b41f7a3aad977802bda4
Bharadwaj Chavan <150778401+codebharatGG@users.noreply.github.com>
2023-11-15 08:12:09 +0000

    practice

--- js/variables.js
+++ js/variables.js
@@ -1,11 +1,20 @@
+// Basics info in web page 
 const accountId = 252525
 let accountName = "Bharadwaj"
-let accountEmail = "bhatradwajchavhan0@gmail.com"
-let accountCity = "Pune"
-let accountState = "Defualt"
+let accountEmail = "bharadwajchavan3@gmail.com"
+let accountPassword = "official@2525"
+let accountCity = "Nandurbar"
+let accountState = "Maharashtra"
+let accountNumber = "77656758"
+let accountDOB = "09/05/2004"
 
-// accountId = 252525
-accountEmail = "bharat0@gmail.com"
-accountCity = "Nandurbar"
-accountName = "Chetan"
-console.table([accountId,accountEmail,accountCity,accountState,accountName])
+ accountName = "Chetan"
+ accountEmail = "chetanchavan3@gmail.com"
+ accountPassword = "chetan@2525"
+ accountCity = "Pune"
+ accountState = "Maharashtra"
+ accountNumber = "77656758"
+ accountDOB = "09/05/2005"
+
+
+console.table([accountId,accountName,accountEmail,accountPassword,accountCity,accountState,accountNumber,accountDOB])
