096a408def329f3192e970f567e3da71edff8004
Bharadwaj Chavan <150778401+codebharatGG@users.noreply.github.com>
2023-11-18 12:42:24 +0000

    comparision

---
+++ js/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 <= 1)
+// console.log(2 != 1)
+// console.log(2 == 1)
+
+// Note:- While comparing,the datatype must be same and if not same then convert them in that same datatype by conversion method!!!
+
+// console.log("2" > 3);  // Different datatype 
+
+// console.log(null > 0);
+// console.log(null == 0);     
+// console.log(null >= 0);      // NOTE:- Comparision convert null to a number, 
+                            //  treating it as 0. That's why null >= 0 is  
+                            //  true and null > 0 is false
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// Note:- In undefined all comparision cases are false  
+
+// Note:- Strick check denoted by triple equal to (===) which means that it will now also check the datatype of the element Example given below
+
+// console.log("2" === 2)
