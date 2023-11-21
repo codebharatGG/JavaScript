77449c312a042a9d1e39e070a5060448fccd9765
Bharadwaj Chavan <150778401+codebharatGG@users.noreply.github.com>
2023-11-21 12:11:43 +0000

    Datatype Summary

---
+++ js/datatypes-summary.js
@@ -0,0 +1,32 @@
+// ******************** SUMMARY ON DATATYPES **********************
+
+//Notes:-  2-Types of datatypes A) Primative B) Non-Primetive (Reference type)
+// A) Primative datatype:- String, Number, Boolean, null, undefined, BigInt, 
+//    Symbol
+// Examples:-
+
+const score = 33
+const scoreValue = 33.4
+let temp = null
+let userEmail;
+const id = Symbol("123")
+const anotherId = Symbol("123")
+console.log(id === anotherId);
+const bigNumber = 3747378770n 
+
+// B) Non-primitive datatype (Reference):- Array, Objects, Functions
+// Examples:- 
+
+const heros =["spiderman","ironman","kaptan marvel"]
+// Curly braces ke aandar objects hote hai!!! In curly braces we write obj in key value pair
+
+let myObj = {
+    name: "Bharadwaj",
+    age: 33,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+
+}
+// To check any type of the datatype we use typeof func
